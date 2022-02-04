78e602dff23bf05a66ffa98190da4c124a0af336
mario9881 <mario_9881@abv.bg>
2022-02-04 12:48:55 +0200

    Print the months as words https://app.asana.com/0/37680704590859/1154216075695513/f

--- inc/templates/admin-template.php
+++ inc/templates/admin-template.php
@@ -46,9 +46,49 @@
 				
 				if ( ! empty( $category_report ) ):
 					foreach( $category_report as $entry ):
+						$monthName = "";
+						switch($entry->month)
+						{
+							case 1:
+								$monthName = "January";
+								break;
+							case 2:
+								$monthName = "February";
+								break;
+							case 3:
+								$monthName = "March";
+								break;
+							case 4:
+								$monthName = "April";
+								break;
+							case 5:
+								$monthName = "May";
+								break;
+							case 6:
+								$monthName = "June";
+								break;
+							case 7:
+								$monthName = "July";
+								break;
+							case 8:
+								$monthName = "August";
+								break;
+							case 9:
+								$monthName = "September";
+								break;
+							case 10:
+								$monthName = "October";
+								break;
+							case 11:
+								$monthName = "November";
+								break;
+							case 12:
+								$monthName = "December";
+								break;
+						}
 			?>
 				<tr>
-					<td><?php printf( '%d.%d', $entry->month, $entry->year ); ?></td>
+					<td><?php printf( "$monthName, $entry->year" ); ?></td>
 					<td><?php echo $entry->count; ?></td>
 				</tr>
 			<?php 
