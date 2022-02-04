4d139e0b6376c38628f05f847499e697b4cf7de7
mario9881 <mario_9881@abv.bg>
2022-02-04 16:29:33 +0200

    Code refactoring, remove submit button, add new hidden option for select menu https://app.asana.com/0/37680704590859/1154216075695513/f

--- dx-category-reports.php
+++ dx-category-reports.php
@@ -8,54 +8,54 @@
  * Text Domain: dxcr
  * License: GPL2
 
- Copyright 2015 mpeshev (email : mpeshev AT devrix DOT com)
+Copyright 2015 mpeshev (email : mpeshev AT devrix DOT com)
 
- This program is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License, version 2, as
- published by the Free Software Foundation.
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as
+published by the Free Software Foundation.
 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
- You should have received a copy of the GNU General Public License
- along with this program; if not, write to the Free Software
- Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- * 
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
  */
 
 if ( ! class_exists( 'DX_Category_Reports' ) ) {
 
 	include_once 'inc/database-time-manager.php';
-	
+
 	/**
 	 * The main Category Reports class
-	 * 
+	 *
 	 * @author nofearinc
 	 *
 	 */
 	class DX_Category_Reports {
-		
+
 		public function __construct() {
 			add_action( 'admin_menu', array( $this, 'register_options_page' ) );
 			add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_media' ) );
 		}
-		
+
 		/**
 		 * Create the options page that does the magic
 		 */
 		public function register_options_page() {
-			add_options_page( __( "DX Category Reports", 'dxcr' ), __( "DX Category Reports", 'dxcr' ), 'edit_themes', 'dx-category-reports', array( $this, 'register_options_page_cb' ) );
+			add_options_page( __( 'DX Category Reports', 'dxcr' ), __( 'DX Category Reports', 'dxcr' ), 'edit_themes', 'dx-category-reports', array( $this, 'register_options_page_cb' ) );
 		}
-		
+
 		public function register_options_page_cb() {
 			include_once 'inc/templates/admin-template.php';
 		}
-		
+
 		/**
 		 * Enqueue the styling for the reports admin page
-		 * 
+		 *
 		 * @param $hook admin hook for the screen options map
 		 */
 		public function enqueue_media( $hook ) {
@@ -65,6 +65,6 @@ if ( ! class_exists( 'DX_Category_Reports' ) ) {
 			}
 		}
 	}
-	
+
 	new DX_Category_Reports();
 }
--- inc/templates/admin-template.php
+++ inc/templates/admin-template.php
@@ -1,103 +1,104 @@
 <div class="wrap">
 	<div id="icon-edit" class="icon32 icon32-base-template"><br></div>
-	
-	<h2><?php _e( "DX Category Reports", 'dxcr' ); ?></h2>
-	
-	<?php 
+
+	<h2><?php _e( 'DX Category Reports', 'dxcr' ); ?></h2>
+
+	<?php
 		$category_id = 0;
-		
-		// Use 0 as a basis, still absint the remote call. 
-		// That happens only in the admin.
-		if ( isset( $_POST['category_id'] ) ) {
-			$category_id = absint( $_POST['category_id'] );
-		}
+
+	// Use 0 as a basis, still absint the remote call.
+	// That happens only in the admin.
+	if ( isset( $_POST['category_id'] ) ) {
+		$category_id = absint( $_POST['category_id'] );
+	}
 	?>
-	
+
 	<form action="" method="post">
 		<div class="reports-settings">
-			<p><?php _e( "Pick a category and fetch the monthly reports for it.", 'dxbase' ); ?></p>
-			
+			<p><?php _e( 'Pick a category and fetch the monthly reports for it.', 'dxbase' ); ?></p>
+
 			<select name="category_id">
-			<?php 
+			<option value="" disabled selected hidden>Choose a category...</option>
+			<?php
 			// Pull all categories. Could be redone for all terms per CPT later.
 			$categories = get_categories();
-	
-			foreach( $categories as $category ): ?>
+
+			foreach ( $categories as $category ) :
+				?>
 				<option value="<?php echo $category->term_id; ?>" <?php selected( $category_id, $category->term_id, true ); ?>><?php echo $category->name; ?></option>
 			<?php endforeach; ?>
 			</select>
-			
-			<input id="report-builder" type="submit" value="Build a report" />
+
 		</div>
 	</form>
-	
+
 	<div class="reports-body">
-	
+
+	<?php
+	if ( 0 !== $category_id ) :
+		?>
 		<table id="reports-table" class="reports-table">
 			<thead>
-				<th><?php _e( "Date", 'dxcr' ); ?></th>
-				<th><?php _e( "Published Posts", 'dxcr' ); ?></th>
+				<th><?php _e( 'Date', 'dxcr' ); ?></th>
+				<th><?php _e( 'Published Posts', 'dxcr' ); ?></th>
 			</thead>
 			<tbody>
-			<?php 
-			if ( 0 != $category_id ):
-				// Fetch the report for the selected category
-				$category_report = DX_Database_Time_Manager::get_cpt_category_report( $category_id );
-				
-				if ( ! empty( $category_report ) ):
-					foreach( $category_report as $entry ):
-						$monthName = "";
-						switch($entry->month)
-						{
-							case 1:
-								$monthName = "January";
-								break;
-							case 2:
-								$monthName = "February";
-								break;
-							case 3:
-								$monthName = "March";
-								break;
-							case 4:
-								$monthName = "April";
-								break;
-							case 5:
-								$monthName = "May";
-								break;
-							case 6:
-								$monthName = "June";
-								break;
-							case 7:
-								$monthName = "July";
-								break;
-							case 8:
-								$monthName = "August";
-								break;
-							case 9:
-								$monthName = "September";
-								break;
-							case 10:
-								$monthName = "October";
-								break;
-							case 11:
-								$monthName = "November";
-								break;
-							case 12:
-								$monthName = "December";
-								break;
-						}
-			?>
+			<?php
+			// Fetch the report for the selected category
+			$category_report = DX_Database_Time_Manager::get_cpt_category_report( $category_id );
+
+			if ( ! empty( $category_report ) ) :
+				foreach ( $category_report as $entry ) :
+					$month_name = '';
+					switch ( $entry->month ) {
+						case 1:
+							$month_name = 'January';
+							break;
+						case 2:
+							$month_name = 'February';
+							break;
+						case 3:
+							$month_name = 'March';
+							break;
+						case 4:
+							$month_name = 'April';
+							break;
+						case 5:
+							$month_name = 'May';
+							break;
+						case 6:
+							$month_name = 'June';
+							break;
+						case 7:
+							$month_name = 'July';
+							break;
+						case 8:
+							$month_name = 'August';
+							break;
+						case 9:
+							$month_name = 'September';
+							break;
+						case 10:
+							$month_name = 'October';
+							break;
+						case 11:
+							$month_name = 'November';
+							break;
+						case 12:
+							$month_name = 'December';
+							break;
+					}
+					?>
 				<tr>
-					<td><?php printf( "$monthName, $entry->year" ); ?></td>
+					<td><?php printf( "$month_name, $entry->year" ); ?></td>
 					<td><?php echo $entry->count; ?></td>
 				</tr>
-			<?php 
+						<?php
 					endforeach;
 				endif;
 			?>
-			<?php 
-			endif; ?>
 			</tbody>
 		</table>
+	<?php endif; ?>
 	</div>
 </div>
